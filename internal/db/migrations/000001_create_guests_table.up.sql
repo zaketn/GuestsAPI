9fa2f52004b5bb96eeca48b992f767332ea2c858
zaketn <specot70@gmail.com>
2024-09-04 02:07:38 +0500

    build: Added unique constraints to email and phone fields in guests table

--- internal/db/migrations/000001_create_guests_table.up.sql
+++ internal/db/migrations/000001_create_guests_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS guests
     id        serial PRIMARY KEY,
     name      VARCHAR(128),
     last_name VARCHAR(128),
-    email     VARCHAR(128),
-    phone     VARCHAR(128),
+    email     VARCHAR(128) UNIQUE,
+    phone     VARCHAR(128) UNIQUE,
     country   VARCHAR(128)
 )
