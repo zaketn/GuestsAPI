000c3259d128025cb406a4b024d5052714da4062
zaketn <specot70@gmail.com>
2024-09-06 00:28:30 +0500

    feat: added response builder

---
+++ pkg/response/options.go
@@ -0,0 +1,54 @@
+package response
+
+import "net/http"
+
+type RespOpts struct {
+	Status  string                 `json:"status,omitempty"`
+	Code    int                    `json:"code,omitempty"`
+	Message string                 `json:"message,omitempty"`
+	Data    map[string]interface{} `json:"data,omitempty"`
+	Error   *Response              `json:"error,omitempty"`
+	Details []map[string]string    `json:"details,omitempty"`
+}
+
+type OptsFunc func(options *RespOpts)
+
+func defaultOptions() RespOpts {
+	return RespOpts{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+	}
+}
+
+func WithCode(code int) OptsFunc {
+	return func(options *RespOpts) {
+		options.Code = code
+		options.Status = http.StatusText(code)
+	}
+}
+
+func WithMessage(message string) OptsFunc {
+	return func(options *RespOpts) {
+		options.Message = message
+	}
+}
+
+func WithData(data interface{}) OptsFunc {
+	return func(options *RespOpts) {
+		dataMap := make(map[string]interface{})
+		dataMap["result"] = data
+	}
+}
+
+func WithNamedData(dataName string, data interface{}) OptsFunc {
+	return func(options *RespOpts) {
+		dataMap := make(map[string]interface{})
+		dataMap[dataName] = data
+	}
+}
+
+func WithDetails(details []map[string]string) OptsFunc {
+	return func(options *RespOpts) {
+		options.Details = details
+	}
+}
---
+++ pkg/response/response.go
@@ -0,0 +1,48 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	RespOpts
+}
+
+func Make(responseOptions ...OptsFunc) []byte {
+	options := defaultOptions()
+
+	for _, fn := range responseOptions {
+		fn(&options)
+	}
+
+	res := &Response{options}
+
+	log.Println(*res)
+
+	if res.Code >= 400 {
+		res = &Response{
+			RespOpts{
+				Error: res,
+			},
+		}
+	}
+
+	return toJson(res)
+}
+
+func toJson(response *Response) []byte {
+	jsonRes, err := json.Marshal(response)
+
+	if err != nil {
+		log.Println(err)
+
+		errRes := Make(WithCode(http.StatusInternalServerError))
+		jsonErrRes, _ := json.Marshal(errRes)
+
+		return jsonErrRes
+	}
+
+	return jsonRes
+}
