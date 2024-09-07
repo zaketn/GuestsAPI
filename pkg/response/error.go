c63525278fe068da6ab0e188eb695ce7233cb89c
zaketn <specot70@gmail.com>
2024-09-08 03:57:43 +0500

    fix: writing body on returning client error

--- pkg/response/error.go
+++ pkg/response/error.go
@@ -9,35 +9,37 @@ func ReturnBadRequestError(w http.ResponseWriter, err error) {
 	log.Println(err)
 
 	res := Make(WithCode(http.StatusBadRequest), WithMessage(err.Error()))
-	jsonError(w, string(res), http.StatusBadRequest)
+	jsonError(w, res, http.StatusBadRequest)
 }
 
 func ReturnNotFoundError(w http.ResponseWriter, err error) {
 	log.Println(err)
 
 	res := Make(WithCode(http.StatusNotFound), WithMessage(err.Error()))
-	jsonError(w, string(res), http.StatusNotFound)
+	jsonError(w, res, http.StatusNotFound)
 }
 
 func ReturnInternalError(w http.ResponseWriter, err error) {
 	log.Println(err)
 
 	res := Make(WithCode(http.StatusInternalServerError))
-	jsonError(w, string(res), http.StatusInternalServerError)
+	jsonError(w, res, http.StatusInternalServerError)
 }
 
 func ReturnError(w http.ResponseWriter, resContent []byte, code int) {
 	h := w.Header()
 	h.Set("Content-Type", "application/json")
 
-	jsonError(w, string(resContent), code)
+	jsonError(w, resContent, code)
 }
 
-func jsonError(w http.ResponseWriter, error string, code int) {
+func jsonError(w http.ResponseWriter, error []byte, code int) {
 	h := w.Header()
 
 	h.Del("Content-Length")
 	h.Set("Content-Type", "application/json")
 	h.Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
+
+	w.Write(error)
 }
