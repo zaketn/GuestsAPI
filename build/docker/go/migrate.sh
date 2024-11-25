3381b777bfc66bfbc19844e1dba5c76839ddd78e
zaketn <specot70@gmail.com>
2024-11-25 19:42:38 +0500

    refactor: move migrations to project root

--- build/docker/go/migrate.sh
+++ build/docker/go/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./migrate -database "postgres://${PG_USER}:${PG_PASSWORD}@db:5432/${PG_DB_NAME}?sslmode=disable" -path ./internal/db/migrations up
+./migrate -database "postgres://${PG_USER}:${PG_PASSWORD}@db:5432/${PG_DB_NAME}?sslmode=disable" -path ./migrations up
 
 exec "$@"
